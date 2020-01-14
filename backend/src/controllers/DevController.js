1d5d9794c18272a09a01f01a908be82bec281fa1
Alvez <joaor4e@gmail.com>
2020-01-14 15:12:04 -0300

    adicionado recurso para impedir cadastros repetitidos

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -3,21 +3,24 @@ const axios = require("axios");
 module.exports = {
     async store(request,response){
         const { github_username, techs, latitude, longitude}=request.body;
-        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-        const { name = login , avatar_url, bio} = apiResponse.data;
-        const techsArray = techs.split(',').map(tech=>tech.trim());
-        const location = {
-            type: 'Point',
-            coordinates: [longitude,latitude]
+        let devs = await dev.findOne({github_username});
+        if(!devs){
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+            const { name = login , avatar_url, bio} = apiResponse.data;
+            const techsArray = techs.split(',').map(tech=>tech.trim());
+            const location = {
+                type: 'Point',
+                coordinates: [longitude,latitude]
+            };
+            devs = await dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location,
+            });
         }
-        const devs = await dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techsArray,
-            location,
-        });
         return response.json(devs);
     }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const {Router} = require("express");
 const routes = Router();
 const DevController = require("./controllers/DevController");
+
 routes.post('/devs', DevController.store);
 module.exports = routes;
